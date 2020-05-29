84c48374742439ed0fe75827993b03dcfbb82af4
gustavo5855 <gustavo.soares.cdc@gmail.com>
2020-05-29 02:02:35 +0000

    [ 28/05/2020 ][ :sparkles: ] servidor 1.15.2

--- rodar.sh
+++ rodar.sh
@@ -3,9 +3,16 @@
 #export SDKMAN_DIR="$HOME/.sdkman"
 #[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 #yes | sdk install java 8.0.252.j9-adpt
+
+# pega pid do antigo processo java
+old_java=$(pgrep -f java)
+
+matar_old_java=$(kill -9 $old_java)
+eval matar_old_java
+
 ./ngrok authtoken 1cMj54TxX3DSjHuDsuvZY2Av73v_79eZpYVvscMVSc7xC15YX
 ./ngrok tcp --region=us 25565 &
 
-java -Xmx2G -Xms2G -jar forge-1.12.2-14.23.5.2847-universal.jar --nogui &
+java -Xmx2G -Xms2G -jar forge-1.15.2-31.2.0.jar --nogui &
 
 

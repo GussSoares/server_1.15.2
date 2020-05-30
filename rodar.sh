8b66ab2caf52ca70a5659567016026552053e225
Ubuntu <gustavo@minecraft-server.fvpxlnhbx2webcw31101vihvif.jx.internal.cloudapp.net>
2020-05-30 04:50:49 +0000

    up

--- rodar.sh
+++ rodar.sh
@@ -4,22 +4,32 @@
 #[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 #yes | sdk install java 8.0.252.j9-adpt
 
+export JVM_ARGS="-Dnashorn.args=--no-deprecation-warning"
+
+home="/home/gustavo/server_1.15.2"
+
+# vai para a raiz do usuario e entra na r
+cd
+cd server_1.15.2
+
 # pega pid do antigo processo java
 old_java=$(pgrep -f java)
-if [ -n $old_java ]
+if [ ${#old_java} -ge 3 ]
 then
 	echo "Processo anterior com id: $old_java"
+	# mata processo antigo
+	kill -9 $old_java
+	echo "Matando processo de id: $old_java"
+else
+	echo "Iniciando Novo Servidor! Aguarde..."
 fi
-# mata processo antigo
-kill -9 $old_java
-echo "Matando processo de id: $old_java"
 
-./ngrok authtoken 1cMj54TxX3DSjHuDsuvZY2Av73v_79eZpYVvscMVSc7xC15YX
-nohup ./ngrok tcp --region=us 25565 &
+#$home/ngrok authtoken 1cMj54TxX3DSjHuDsuvZY2Av73v_79eZpYVvscMVSc7xC15YX
+#nohup $home/ngrok tcp --region=us 25565 &
 
 echo ""
 ip=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url')
-echo "##### IP DO SERVIDOR #####\n"
-echo "$ip"
+echo "IP DO SERVIDOR: $ip"
+echo ""
 
-java -Xmx2G -Xms2G -jar forge-1.15.2-31.2.0.jar --nogui
+java -Xmx2G -Xms2G -jar /home/gustavo/server_1.15.2/forge-1.15.2-31.2.0.jar --nogui &
